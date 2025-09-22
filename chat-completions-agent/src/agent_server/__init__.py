d54d09cbc3777db3ee28493c0d4889607344123d
Bryan Qiu <55931436+bbqiu@users.noreply.github.com>
2025-09-21 23:35:12 -0700

    deleting (#20)
    Signed-off-by: Bryan Qiu <bryan.qiu@databricks.com>

--- chat-completions-agent/src/agent_server/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Agent server package for MLflow-compatible endpoints."""
-
-from .server import AgentServer, create_server, invoke, parse_server_args, stream
-
-__all__ = ["AgentServer", "create_server", "invoke", "stream", "parse_server_args"]
